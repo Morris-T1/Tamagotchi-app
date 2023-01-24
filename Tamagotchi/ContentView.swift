410a9e0c2234fb9a7c425671af440d092aa5683d
Morris-T1 <morris.t@etoncollege.org.uk>
2023-01-24 10:24:01 +0000

    Initial Commit

---
+++ Tamagotchi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tamagotchi
+//
+//  Created by Morris, Fenston (AGDF) on 24/01/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tamagotchi/TamagotchiApp.swift
@@ -0,0 +1,17 @@
+//
+//  TamagotchiApp.swift
+//  Tamagotchi
+//
+//  Created by Morris, Fenston (AGDF) on 24/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct TamagotchiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
